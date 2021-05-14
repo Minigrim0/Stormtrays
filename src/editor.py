f3fb95eba64594b47edcb23396bf962a699f81d2
Minigrim0 <grimauflorent@gmail.com>
2021-05-14 10:15:17 +0200

    [Upd] Buttons

--- src/editor.py
+++ src/editor.py
@@ -11,18 +11,20 @@ import src.constantes as consts
 
 class Editor:
     def __init__(self):
-        self.UI = EditorUI()
-        self.UI.eraseButton.callback = self.erase
-        self.UI.changeBackgroundButton.callback = self.changeBackground
-        self.UI.loadButton.callback = self.loadLevel
-        self.UI.saveButton.callback = self.save
+        self.level = Niveau()
+        self.UI = EditorUI(self.level)
+        self.UI.buttons["eraseButton"].callback = self.erase
+        self.UI.buttons["changeBackgroundButton"].callback = self.changeBackground
+        self.UI.buttons["loadButton"].callback = self.loadLevel
+        self.UI.buttons["saveButton"].callback = self.save
+        for buttonID in ["c1", "t2", "t1", "x1", "p1", "v1", "k1", "QG"]:
+            self.UI.buttons[buttonID].callback = self.setChoice, buttonID
 
         self.running = True
 
-        self.choix = "  "
+        self.choice = None
         self.rot = 0
-        self.niveau = Niveau()
-        self.possouris = (0, 0)
+        self.mousePosition = (0, 0)
 
     def run(self, screen: Screen):
         while self.running:
@@ -34,80 +36,80 @@ class Editor:
     def handleEvent(self, screen: Screen, event: pygame.event.Event):
         if event.type == pygame.locals.MOUSEBUTTONDOWN:
             if event.button == 1:
-                self.choix = self.UI.update(screen, event, self.niveau, self.choix)
+                self.UI.update(event)
 
+                if self.choice is not None:
                     x = event.pos[0] // 64
                     y = event.pos[1] // 64
+                    if self.choice == "p1":
+                        self.level.map[x, y] = "  ", 0
+                    elif self.choice == "QG":
+                        self.QGPos = (x * 64, y * 64)
                     else:
-                        self.niveau.tableau[x, y] = self.choix, self.rot
+                        self.level.map[x, y] = self.choice, self.rot
 
-            if event.button == 3 and self.choix != "  ":
+            if event.button == 3 and self.choice != "  ":
                 self.rot = (self.rot + 90) % 360
 
         if event.type == pygame.locals.MOUSEMOTION:
-            self.possouris = (event.pos[0], event.pos[1])
+            self.mousePosition = (event.pos[0], event.pos[1])
 
+            if event.buttons[0] == 1 and self.choice != "  ":
                 x = event.pos[0] // 64
                 y = event.pos[1] // 64
-                        self.niveau.tableau[x, y] = self.choix, self.rot
+                if self.choice == "p1":
+                    self.level.map[x, y] = "  ", 0
+                else:
+                    self.level.map[x, y] = self.choice, self.rot
 
     def draw(self, screen):
-        self.niveau.afficheE(screen, self.UI.fond)
+        self.level.afficheE(screen, self.UI.fond)
         self.UI.draw(screen)
 
         if self.UI.QGPos:
             screen.blit(self.UI.QGImg, (self.UI.QGPos))
 
-        if self.choix != "  ":
+        if self.choice is not None:
             screen.blit(
-                self.niveau.imgE[self.choix, self.rot],
-                ((self.possouris[0] // 64) * 64, (self.possouris[1] // 64) * 64)
+                self.level.editorImage[self.choice, self.rot],
+                ((self.mousePosition[0] // 64) * 64, (self.mousePosition[1] // 64) * 64),
             )
-
-        for v in self.UI.rect:
-            screen.blit(
-                self.niveau.imgE[v, 0], (self.UI.rect[v].x, self.UI.rect[v].y))
 
         screen.flip()
 
     def erase(self):
-        self.niveau.videtab()
-        self.choix = "  "
+        self.level.empty()
+        self.choice = "  "
 
     def changeBackground(self):
-        filename = filedialog.askopenfilename(
-            initialdir="img/Fonds", defaultextension=".png")
+        filename = filedialog.askopenfilename(initialdir="img/Fonds", defaultextension=".png")
         if filename:
             self.UI.fond_Edit = os.path.relpath(filename)
             self.UI.fond = pygame.image.load(self.UI.fond_Edit).convert_alpha()
 
-        self.choix = "  "
+        self.choice = "  "
 
     def loadLevel(self):
-        filename = filedialog.askopenfilename(
-            initialdir="level", defaultextension=".txt")
+        filename = filedialog.askopenfilename(initialdir="level", defaultextension=".txt")
         if filename:
-            self.niveau.construit(filename)
-        self.choix = "  "
+            self.level.construit(filename)
+        self.choice = "  "
 
     def save(self):
-        filename = filedialog.asksaveasfilename(
-            initialdir="level", defaultextension=".txt")
+        filename = filedialog.asksaveasfilename(initialdir="level", defaultextension=".txt")
         if filename:
-            self.niveau.sauve(filename)
-            self.niveau.sauveF(filename, self.fond_Edit, self.QGPos)
+            self.level.sauve(filename)
+            self.level.sauveF(filename, self.fond_Edit, self.QGPos)
             # self.niveau.affiche(screen, self.fond)
             pygame.display.flip()
-            arect = pygame.Rect(
-                0, 0, consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT)
+            arect = pygame.Rect(0, 0, consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT)
             sub = screen.subsurface(arect)
-            sub = pygame.transform.scale(sub, (39*5, 22*5))
+            sub = pygame.transform.scale(sub, (39 * 5, 22 * 5))
             dirname, filename = os.path.split(filename)
             filename, ext = os.path.splitext(filename)
-            pygame.image.save(
-                sub, os.path.join(
-                    dirname, "mininiveau", filename+".png"
-                )
-            )
+            pygame.image.save(sub, os.path.join(dirname, "mininiveau", filename + ".png"))
 
-        self.choix = "  "
+        self.choice = "  "
+
+    def setChoice(self, choice):
+        self.choice = choice
