10cd200db9e4ee9b80d17a58289b981ce1be264e
Minigrim0 <grimauflorent@gmail.com>
2021-06-28 18:05:41 +0200

    [Fix] inconsistent returns

--- src/ennemis.py
+++ src/ennemis.py
@@ -7,7 +7,6 @@ import glob
 
 
 class Ennemi_IG(object):
-
     def __init__(self, FichierEnnemi):
         self.posx = 0
         self.posy = 0
@@ -28,16 +27,14 @@ class Ennemi_IG(object):
         self.propriete = json.loads(contenu)
 
         self.Name = self.propriete["Name"]
-        self.image2Scale = pygame.image.load(
-            self.propriete["Img"]).convert_alpha()
+        self.image2Scale = pygame.image.load(self.propriete["Img"]).convert_alpha()
         self.meurt = pygame.mixer.Sound(self.propriete["DeathSound"])
         self.vie = self.propriete["LifePts"]
         self.vie_bas = self.propriete["LifePts"]
         self.vitesse = self.propriete["Speed"]
         self.Height = self.propriete["Height"]
-        self.image = pygame.transform.scale(
-            self.image2Scale, (self.Height, self.Height))
-        self.Vie_Rect_Arriere = pygame.Surface((self.vie*(60/self.vie), 3))
+        self.image = pygame.transform.scale(self.image2Scale, (self.Height, self.Height))
+        self.Vie_Rect_Arriere = pygame.Surface((self.vie * (60 / self.vie), 3))
         self.Vie_Rect_Arriere.fill((255, 0, 0))
 
         for filename in glob.glob(self.propriete["ImgFolder"]):
@@ -47,8 +44,7 @@ class Ennemi_IG(object):
 
         for filename in glob.glob(self.propriete["ImgFolderRet"]):
             ImgR2Scale = pygame.image.load(filename).convert_alpha()
-            ImgR = pygame.transform.scale(
-                ImgR2Scale, (self.Height, self.Height))
+            ImgR = pygame.transform.scale(ImgR2Scale, (self.Height, self.Height))
             self.tab_ret.append(ImgR)
 
     def pose_ennemi(self, tableau, fenetre):
@@ -59,9 +55,9 @@ class Ennemi_IG(object):
         x = 0
         for y in range(11):
             position = tableau[x, y]
-            if position == ('c1', 0):
+            if position == ("c1", 0):
                 self.posy = y
-                self.PosAbsolue = (0, y*64)
+                self.PosAbsolue = (0, y * 64)
                 self.HitBox = pygame.Rect((0, y), (64, 64))
 
     def BlitInPlace(self, screen):
@@ -71,7 +67,7 @@ class Ennemi_IG(object):
     def bouge(self, tableau, fenetre, niveau, Liste_Mechants, coin, King):
 
         try:
-            self.Vie_Rect = pygame.Surface((self.vie*(60/self.vie_bas), 3))
+            self.Vie_Rect = pygame.Surface((self.vie * (60 / self.vie_bas), 3))
         except Exception as e:
             print("Warning :", e)
         self.Vie_Rect.fill((0, 255, 0))
@@ -90,30 +86,30 @@ class Ennemi_IG(object):
         self.count += 1
 
         # PLUS EN X  (->)
-        if position == ('c1', 0) or position == ('t1', 180) or position == ('t2', 270):
+        if position == ("c1", 0) or position == ("t1", 180) or position == ("t2", 270):
             self.Dir_x = 1
             self.Dir_y = 0
             self.Returned = False
 
         # MOINS EN Y (^)
-        elif position == ('c1', 90) or position == ('t1', 270) or position == ('t2', 0):
+        elif position == ("c1", 90) or position == ("t1", 270) or position == ("t2", 0):
             self.Dir_x = 0
             self.Dir_y = -1
             self.Returned = False
 
         # MOINS EN X (<-)
-        elif position == ('c1', 180) or position == ('t1', 0) or position == ('t2', 90):
+        elif position == ("c1", 180) or position == ("t1", 0) or position == ("t2", 90):
             self.Dir_x = -1
             self.Dir_y = 0
             self.Returned = True
 
         # PLUS EN Y (v)
-        elif position == ('c1', 270) or position == ('t1', 90) or position == ('t2', 180):
+        elif position == ("c1", 270) or position == ("t1", 90) or position == ("t2", 180):
             self.Dir_x = 0
             self.Dir_y = 1
             self.Returned = True
 
-        elif position == ('x1', 0) or position == ('x1', 90) or position == ('x1', 180) or position == ('x1', 270):
+        elif position == ("x1", 0) or position == ("x1", 90) or position == ("x1", 180) or position == ("x1", 270):
             self.Dir_x = self.Dir_x
             self.Dir_y = self.Dir_y
             self.Returned = self.Returned
@@ -122,7 +118,7 @@ class Ennemi_IG(object):
             print("Position erronée")
             self.pose_ennemi(tableau, fenetre)
 
-        if self.count == round(64/self.vitesse):
+        if self.count == round(64 / self.vitesse):
 
             self.count = 0
 
@@ -130,16 +126,15 @@ class Ennemi_IG(object):
             self.posy += self.Dir_y
 
             if self.BlitLife:
-                fenetre.blit(
-                    self.Vie_Rect_Arriere, (self.posx*64, self.posy*64))
-                fenetre.blit(self.Vie_Rect, (self.posx*64, self.posy*64))
-            fenetre.blit(self.image, (self.posx*64, self.posy*64))
+                fenetre.blit(self.Vie_Rect_Arriere, (self.posx * 64, self.posy * 64))
+                fenetre.blit(self.Vie_Rect, (self.posx * 64, self.posy * 64))
+            fenetre.blit(self.image, (self.posx * 64, self.posy * 64))
 
         else:
 
             self.PosAbsolue = (
-                self.posx*64+(self.count*self.vitesse*self.Dir_x),
-                self.posy*64+(self.count*self.vitesse*self.Dir_y)
+                self.posx * 64 + (self.count * self.vitesse * self.Dir_x),
+                self.posy * 64 + (self.count * self.vitesse * self.Dir_y),
             )
             if self.BlitLife:
                 fenetre.blit(self.Vie_Rect_Arriere, self.PosAbsolue)
@@ -150,7 +145,7 @@ class Ennemi_IG(object):
             self.anim()
 
         if self.posx == niveau.pos_Chateau[0] and self.posy == niveau.pos_Chateau[1]:
-            niveau.Vie_Chateau -= self.vie//1.5
+            niveau.Vie_Chateau -= self.vie // 1.5
             self.enleve_vie(2000, Liste_Mechants, self, niveau, coin, King)
 
     def enleve_vie(self, viemoins, liste_mech, ennemi, niveau, coin, King):
@@ -166,36 +161,27 @@ class Ennemi_IG(object):
             self.meurt.play()
             if King.capacite1 is True:
                 FlyingGold = GoldAnim(
-                    (
-                        self.PosAbsolue[0] + self.Height//2,
-                        self.PosAbsolue[1] + self.Height//2
-                    ),
-                    self.vie_bas
+                    (self.PosAbsolue[0] + self.Height // 2, self.PosAbsolue[1] + self.Height // 2), self.vie_bas
                 )
                 constantes.GoldGained[0] += self.vie_bas
                 niveau.GoldTab.append(FlyingGold)
                 niveau.gold += self.vie_bas
 
             else:
-                FlyingGold = GoldAnim(
-                    (
-                        self.PosAbsolue[0] + 32,
-                        self.PosAbsolue[1] + 32
-                    ),
-                    self.vie_bas//2
-                )
-                constantes.GoldGained[0] += self.vie_bas//2
+                FlyingGold = GoldAnim((self.PosAbsolue[0] + 32, self.PosAbsolue[1] + 32), self.vie_bas // 2)
+                constantes.GoldGained[0] += self.vie_bas // 2
                 niveau.GoldTab.append(FlyingGold)
-                niveau.gold += self.vie_bas//2
+                niveau.gold += self.vie_bas // 2
 
             niveau.Nombre_Ennemis_Tue += 1
             return True
+        return False
 
     def anim(self):
         self.i += 1
         if self.i == 12:
             self.i = 0
         if self.Returned:
-            self.image = self.tab_ret[self.i//2]
+            self.image = self.tab_ret[self.i // 2]
         else:
-            self.image = self.tab[self.i//2]
+            self.image = self.tab[self.i // 2]
--- src/mobsInMenu.py
+++ src/mobsInMenu.py
@@ -8,7 +8,6 @@ from src.gold import GoldAnim
 
 
 class EnnemiIM(object):
-
     def __init__(self, FichierEnnemi):
 
         self.posx = 0
@@ -28,16 +27,14 @@ class EnnemiIM(object):
         contenu = f.read()
         self.propriete = json.loads(contenu)
 
-        self.image2Scale = pygame.image.load(
-            self.propriete["Img"]).convert_alpha()
+        self.image2Scale = pygame.image.load(self.propriete["Img"]).convert_alpha()
         self.meurt = pygame.mixer.Sound(self.propriete["DeathSound"])
         self.vie = 20
         self.vie_bas = 20
         self.vitesse = self.propriete["Speed"]
         self.Height = self.propriete["Height"]
-        self.image = pygame.transform.scale(
-            self.image2Scale, (self.Height, self.Height))
-        self.Vie_Rect_Arriere = pygame.Surface((self.vie*(60/self.vie), 3))
+        self.image = pygame.transform.scale(self.image2Scale, (self.Height, self.Height))
+        self.Vie_Rect_Arriere = pygame.Surface((self.vie * (60 / self.vie), 3))
         self.Vie_Rect_Arriere.fill((255, 0, 0))
 
         for filename in glob.glob(self.propriete["ImgFolder"]):
@@ -47,8 +44,7 @@ class EnnemiIM(object):
 
         for filename in glob.glob(self.propriete["ImgFolderRet"]):
             ImgR2Scale = pygame.image.load(filename).convert_alpha()
-            ImgR = pygame.transform.scale(
-                ImgR2Scale, (self.Height, self.Height))
+            ImgR = pygame.transform.scale(ImgR2Scale, (self.Height, self.Height))
             self.tab_ret.append(ImgR)
 
     def pose_ennemi(self, fenetre):
@@ -60,7 +56,7 @@ class EnnemiIM(object):
 
     def vit(self, fenetre):
 
-        self.Vie_Rect = pygame.Surface((self.vie*(60/self.vie_bas), 3))
+        self.Vie_Rect = pygame.Surface((self.vie * (60 / self.vie_bas), 3))
         self.Vie_Rect.fill((0, 255, 0))
 
         if self.IsAttacked:
@@ -74,25 +70,22 @@ class EnnemiIM(object):
 
         self.PosAbsolue = (self.posx, self.posy)
 
-        dist = math.sqrt(
-            ((self.posx - self.TargetX) ** 2)
-            + ((self.posy - self.TargetY) ** 2)
-        )
+        dist = math.sqrt(((self.posx - self.TargetX) ** 2) + ((self.posy - self.TargetY) ** 2))
 
-        if dist > self.vitesse*10:
+        if dist > self.vitesse * 10:
             delta_y = self.TargetY - self.posy
             delta_x = self.TargetX - self.posx
 
             if delta_x != 0:
-                angle = math.atan(delta_y/delta_x)
+                angle = math.atan(delta_y / delta_x)
             else:
                 if delta_y < 0:
-                    angle = -math.pi/2
+                    angle = -math.pi / 2
                 else:
-                    angle = math.pi/2
+                    angle = math.pi / 2
 
             if delta_x < 0:
-                angle = angle+math.pi
+                angle = angle + math.pi
                 self.Returned = True
 
             else:
@@ -101,8 +94,8 @@ class EnnemiIM(object):
             self.posx_Old = self.posx
             self.posy_Old = self.posy
 
-            self.posy = self.posy + math.sin(angle)*self.vitesse
-            self.posx = self.posx + math.cos(angle)*self.vitesse
+            self.posy = self.posy + math.sin(angle) * self.vitesse
+            self.posx = self.posx + math.cos(angle) * self.vitesse
 
             self.anim()
 
@@ -127,28 +120,23 @@ class EnnemiIM(object):
             self.meurt.play()
             if King.capacite1 is True:
                 FlyingGold = GoldAnim(
-                    self.PosAbsolue[0] + self.Height//2,
-                    self.PosAbsolue[1] + self.Height//2,
-                    self.vie_bas
+                    self.PosAbsolue[0] + self.Height // 2, self.PosAbsolue[1] + self.Height // 2, self.vie_bas
                 )
                 niveau.GoldTab.append(FlyingGold)
                 niveau.gold += self.vie_bas
             else:
-                FlyingGold = GoldAnim(
-                    self.PosAbsolue[0] + 32,
-                    self.PosAbsolue[1] + 32,
-                    self.vie_bas//2
-                )
+                FlyingGold = GoldAnim(self.PosAbsolue[0] + 32, self.PosAbsolue[1] + 32, self.vie_bas // 2)
                 niveau.GoldTab.append(FlyingGold)
-                niveau.gold += self.vie_bas//2
+                niveau.gold += self.vie_bas // 2
             niveau.Nombre_Ennemis_Tue += 1
             return True
+        return False
 
     def anim(self):
         self.i += 1
         if self.i == 12:
             self.i = 0
         if self.Returned:
-            self.image = self.tab_ret[self.i//2]
+            self.image = self.tab_ret[self.i // 2]
         else:
-            self.image = self.tab[self.i//2]
+            self.image = self.tab[self.i // 2]
