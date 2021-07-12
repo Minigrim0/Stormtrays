3c4fc543f029d22838d1db9b96bef9a03ef427db
Minigrim0 <grimauflorent@gmail.com>
2021-07-12 23:23:20 +0200

    [Add] Menu class

---
+++ menus/menu.py
@@ -0,0 +1,27 @@
+from pygame.locals import MOUSEBUTTONDOWN
+
+from UI.components.button import Button
+from models.screen import Screen
+
+
+class Menu:
+    def __init__(self, screen: Screen):
+        self.buttons: [Button] = []
+        self.screen = screen
+
+    def handleEvent(self):
+        for event in self.screen.getEvent():
+            if event.type == MOUSEBUTTONDOWN:
+                for button in self.buttons:
+                    button.click(event.pos)
+
+            yield event
+
+    def draw(self):
+        for button in self.buttons:
+            button.draw(self.screen)
+
+    def loop(self):
+        from singleton.game import Game
+
+        Game.getInstance().play_music()
