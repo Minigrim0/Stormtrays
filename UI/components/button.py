30580902960f0f40afebd79b720ac551ab486f50
Minigrim0 <grimauflorent@gmail.com>
2021-07-12 23:23:45 +0200

    [Upd] moved button to UI components

--- UI/components/button.py
+++ UI/components/button.py
@@ -12,30 +12,15 @@ class Button:
         self.callback: callable = None
 
     def draw(self, screen):
-        """Draws the button on the screen
-
-        Args:
-            screen (Screen): The object to draw on
-        """
+        """Draws the button on the screen"""
         screen.blit(self.image, self.pos)
 
     def collide(self, pos: tuple):
-        """Checks whether the position is colliding the button
-
-        Args:
-            pos (tuple): The position to test
-
-        Returns:
-            bool: whether the pos is colliding the button or not
-        """
+        """Checks whether the position is colliding the button"""
         return self.rect.collidepoint(pos)
 
     def click(self, pos: tuple):
-        """Execute the callback if the position collide the button
-
-        Args:
-            pos (tuple): the position to click on
-        """
+        """Execute the callback if the position collide the button"""
         if self.collide(pos) and self.callback is not None:
             if type(self.callback) is tuple:
                 self.callback[0](*self.callback[1:])
