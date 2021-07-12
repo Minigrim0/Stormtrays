2eed161f4276dc523d097b4cb25998e7e042db90
Minigrim0 <grimauflorent@gmail.com>
2021-07-12 13:37:08 +0200

    [Fix] documentation + call super init

--- exceptions/invalidPositionException.py
+++ exceptions/invalidPositionException.py
@@ -1,5 +1,8 @@
 class InvalidPositionException(Exception):
+    """An exception that occurs when the position of an event is not correct"""
+
     def __init__(self, message):
+        super().__init__()
         self.message = message
 
     def __str__(self):
