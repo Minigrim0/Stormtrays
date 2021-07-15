6d8a52a25caea19968b77380d5658653506a747a
Minigrim0 <grimauflorent@gmail.com>
2021-07-15 15:23:38 +0200

    [Fix] imports

--- Editor.py
+++ Editor.py
@@ -4,18 +4,13 @@ import pygame.locals
 import tkinter
 
 import src.constantes as constantes
-from src.screen import Screen
 
-from src.editor import Editor
+from models.screen import Screen
+from models.editor import Editor
 
 pygame.init()
 
-screen = Screen(
-    (constantes.WINDOW_WIDTH+158, constantes.WINDOW_HEIGHT),
-    "Stormtray's Editor",
-    "img/Icon.png",
-    False
-)
+screen = Screen((constantes.WINDOW_WIDTH + 158, constantes.WINDOW_HEIGHT), "Stormtray's Editor", "img/Icon.png", False)
 
 root = tkinter.Tk()
 root.withdraw()
