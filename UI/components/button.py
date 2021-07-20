9012a4e0bae8b4b1abdb9d39ca5fcaa5de1399d2
Minigrim0 <grimauflorent@gmail.com>
2021-07-20 13:25:09 +0200

    [Add] button building

--- UI/components/button.py
+++ UI/components/button.py
@@ -1,19 +1,50 @@
-import pygame
+import pygame as pg
 
 
 class Button:
     """A button that can be clicked and may induce a callback"""
 
-    def __init__(
-        self, pos: tuple, size: tuple, image: pygame.Surface = None, callback: callable = None, *cargs, **ckwargs
-    ):
+    def __init__(self, pos: tuple, size: tuple, image: pg.Surface = None, callback: callable = None, *cargs, **ckwargs):
         self.pos = pos
         self.size = size
         self.image = image
-        self.rect = pygame.Rect(self.pos, self.size)
+        self.rect = pg.Rect(self.pos, self.size)
         self.callback: callable = callback
         self.cargs = cargs
         self.ckwargs = ckwargs
+
+    def toPos(self, position: tuple, insize: tuple, outsize: tuple):
+        final_position = []
+        for axis, var in enumerate(position):
+            if type(var) is int:
+                final_position.append(var)
+            elif type(var) is str:
+                if var == "CENTER":
+                    outlength = outsize[axis]
+                    inlength = insize[axis]
+                    final_position.append((outlength - inlength) // 2)
+                if var == "TOP":
+                    final_position.append(0)
+                if var == "BOTTOM":
+                    outlength = outsize[axis]
+                    inlength = insize[axis]
+                    final_position.append(outlength - inlength)
+
+        return tuple(final_position)
+
+    def build(
+        self,
+        text: str,
+        font: pg.font.Font,
+        text_position: tuple,
+        background: pg.Surface = None,
+        text_color: tuple = (0, 0, 0),
+    ):
+        if background is not None:
+            self.image = background
+
+        text = font.render(text, 1, text_color)
+        self.image.blit(text, self.toPos(text_position, text.get_size(), self.image.get_size()))
 
     def setCallback(self, callback: callable, *cargs, **ckwargs):
         """Sets the button callback with its parameters"""
