9376dfb45d746eea9b6ce58f3d42af6eda3dbb28
Minigrim0 <grimauflorent@gmail.com>
2021-07-25 12:27:47 +0200

    [Add] eventual trigger for image animation + fix drawing

--- UI/components/imageAnimation.py
+++ UI/components/imageAnimation.py
@@ -8,21 +8,30 @@ from models.screen import Screen
 
 
 class ImageAnimation:
-    def __init__(self, position: tuple, flippable: bool = False):
+    """An animation of images"""
+
+    def __init__(self, folder_path: str = None, flippable: bool = False, callback: callable = None):
         self.images: list(pg.Surface) = []
         self.images_flipped: list(pg.Surface) = []
 
-        self.position: tuple = position
-
         self.flippable: bool = flippable
         self.flipped = False
-        self.last_step: int = None  # The time since last step
+        self.last_step: int = 0  # The time since last step
 
         self.step: int = 0
         self.playing: bool = True
-        self.spleed: int = 1  # 1 image par seconde
+        self.speed: int = 1  # 1 image par seconde
+        self.trigger = callback
+
+        if folder_path is not None:
+            self.loadFolder(folder_path)
 
     def loadFolder(self, folder_path: str):
+        """Loads an animation from a folder
+
+        Args:
+            folder_path (str): The path to the animation folder
+        """
         setup_file = os.path.join(folder_path, "setup.json")
         if not os.path.exists(setup_file):
             logging.warning("No setup file found for {}, trying fuzzy load".format(folder_path))
@@ -59,12 +68,14 @@ class ImageAnimation:
     def update(self, timeElapsed):
         self.last_step += timeElapsed
         if self.last_step > (1 / self.speed):
+            if self.trigger is not None:
+                self.trigger()
             self.last_step = 0
             self.step += 1
             self.step %= len(self.images)
 
-    def draw(self, screen: Screen):
+    def draw(self, screen: Screen, position: tuple):
         if self.flipped:
-            screen.blit(self.images_flipped, self.position)
+            screen.blit(self.images_flipped[self.step], position)
         else:
-            screen.blit(self.images, self.position)
+            screen.blit(self.images[self.step], position)
