f1e785ca15ab1e64ae7540dab5a12c91340ed970
Minigrim0 <grimauflorent@gmail.com>
2021-07-25 21:44:40 +0200

    [Del] useless classes

--- src/classes.py
+++
@@ -1,7 +0,0 @@
-class Levels:
-    """A class to keep information on a level"""
-
-    def __init__(self, file, img, nivrect):
-        self.File = file
-        self.Img = img
-        self.Nivrect = nivrect
