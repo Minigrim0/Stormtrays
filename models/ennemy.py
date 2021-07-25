1001217cbc7fc9ab4864d9fea630834e3a90766f
Minigrim0 <grimauflorent@gmail.com>
2021-07-25 14:53:56 +0200

    [Fix] invoke with weights

--- models/ennemy.py
+++ models/ennemy.py
@@ -6,6 +6,7 @@ import random
 from src.ennemy import EnnemyDO
 
 from models.gameOptions import GameOptions
+from models.level import Level
 
 
 class Ennemy:
@@ -25,8 +26,8 @@ class Ennemy:
         Ennemy.instance = self
 
         self.ennemies: [EnnemyDO] = []
-        self.available_ennemies: {str: {}} = {}
-        self.ennemies_weight: [int] = []
+        self.available_ennemies: [{}] = []
+        self.ennemies_weights: list(int) = []
         self.load()
 
     def load(self):
@@ -34,50 +35,25 @@ class Ennemy:
         for ennemyFile in glob.glob(options.fullPath("ennemies", "*.json")):
             with open(ennemyFile) as ennemyInfo:
                 data = json.load(ennemyInfo)
-                self.ennemies_weight.append(data["weight"])
-                self.available_ennemies[os.path.splitext(os.path.split(ennemyFile)[1])[0]] = data
+                self.available_ennemies.append(data)
+                self.ennemies_weights.append(data["weight"])
 
     def update(self):
-        # if self.level.Nombre_Ennemis_Tue >= 5000:
-        #     double_invoque = True
-
-        invoke = random.random() * 1
+        invoke = random.random() * 100
         if invoke <= 1:
             self.invoke()
+        
+
+        for ennemy in self.ennemies:
+            ennemy.update(screen)
+
+    def draw(self, screen):
+        for ennemy in self.ennemies:
+            ennemy.draw(screen)
 
     def invoke(self):
-        invoque = random.randrange(10)
-        if invoque == 0:
-            ennemi = EnnemyDO("assets/ennemies/Orc.json")
-            ennemi.pose_ennemi(self.level.map)
-            self.ennemies.append(ennemi)
-        elif invoque in (1, 2):
-            ennemi = EnnemyDO("assets/ennemies/Goblin.json")
-            ennemi.pose_ennemi(self.level.map)
-            self.ennemies.append(ennemi)
-        elif invoque == 3:
-            ennemi = EnnemyDO("assets/ennemies/Dwarf.json")
-            ennemi.pose_ennemi(self.level.map)
-            self.ennemies.append(ennemi)
-        elif invoque == 4:
-            ennemi = EnnemyDO("assets/ennemies/Knight.json")
-            ennemi.pose_ennemi(self.level.map)
-            self.ennemies.append(ennemi)
-        elif invoque == 5:
-            ennemi = EnnemyDO("assets/ennemies/Ghost.json")
-            ennemi.pose_ennemi(self.level.map)
-            self.ennemies.append(ennemi)
-        elif invoque == 6:
-            ennemi = EnnemyDO("assets/ennemies/Golem.json")
-            ennemi.pose_ennemi(self.level.map)
-            self.ennemies.append(ennemi)
-        elif invoque == 7:
-            invoque = random.randrange(5)
-            if invoque == 0:
-                ennemi = EnnemyDO("assets/ennemies/Dragon.json")
-                ennemi.pose_ennemi(self.level.map)
-                self.ennemies.append(ennemi)
-        elif invoque in (8, 9, 10):
-            ennemi = EnnemyDO("assets/ennemies/Wolf.json")
-            ennemi.pose_ennemi(self.level.map)
-            self.ennemies.append(ennemi)
+        ennemy = random.choices(self.available_ennemies, weights=self.ennemies_weights)[0]
+
+        ennemi = EnnemyDO(ennemy)
+        ennemi.pose_ennemi(Level.getInstance().map)
+        self.ennemies.append(ennemi)
