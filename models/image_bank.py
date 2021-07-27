a0d298dca0ffd2842257aef2c509b4762490fcdd
Minigrim0 <grimauflorent@gmail.com>
2021-07-27 12:18:41 +0200

    [Add] Image Bank singleton

---
+++ models/image_bank.py
@@ -0,0 +1,26 @@
+class ImageBank:
+    """Represents a bank of images, to avoid loading images multiple times"""
+
+    instance = None
+
+    @staticmethod
+    def getInstance():
+        if ImageBank.instance is None:
+            ImageBank()
+        return ImageBank.instance
+
+    def __init__(self):
+        if ImageBank.instance is not None:
+            raise RuntimeError("Trying to instanciate second object from singleton class")
+        ImageBank.instance = self
+
+        self.images: {str: any} = {}
+
+    def __getitem__(self, name: str):
+        return self.images[name]
+
+    def exists(self, name: str):
+        return name in self.images.keys()
+
+    def set(self, name: str, value):
+        self.images[name] = value
