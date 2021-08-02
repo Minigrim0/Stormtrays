91a4a74d0e5355b6500ec970802a72bfb0a761a6
Minigrim0 <grimauflorent@gmail.com>
2021-08-02 21:46:04 +0200

    [Add] tower documentation

--- src/tower.py
+++ src/tower.py
@@ -38,23 +38,27 @@ class TowerDO:
         )
 
     @property
-    def flipped(self):
+    def flipped(self) -> bool:
+        """Returns a boolean indicating whether the tower is flipped or not"""
         return self.animation.flipped
 
     @property
-    def absolute_position(self):
+    def absolute_position(self) -> tuple:
+        """Returns the position on screen"""
         return (
             self.position[0] * 64,
             self.position[1] * 64,
         )
 
     def _targetInRange(self, target: EnnemyDO = None):
+        """Check if the given or the current target is in range of the tower"""
         target = target if target is not None else self.target
 
         t = self.timeToImpact(target)
         return target.alive and (t - self.t0) * self.projectile_model["speed"] <= self.range * 64
 
     def _acquireTarget(self):
+        """Finds the first in-range ennemy to target it"""
         for ennemy in Ennemy.getInstance().getEnnemyList():
             if self._targetInRange(ennemy):
                 self.target = ennemy
@@ -81,6 +85,7 @@ class TowerDO:
             self.animation.draw(screen, self.position)
 
     def update(self, timeElapsed: float):
+        """Updates the in-game towers and the tower menu"""
         if self.placed:
             if self.target is not None:
                 if self._targetInRange():
@@ -101,37 +106,36 @@ class TowerDO:
             self.timeToImpact(self.target)
         )
 
-    def timeToImpact(self, target):
-        """Calculates the time until impact
+    def timeToImpact(self, target) -> float:
+        """
+        Returns the time until impact for an hypothetic projectile
 
-        Args:
-            ennemi ([type]): [description]
+        Triangle tower/ennemy/impact
+        b² = a²c² - 2ac*cos(ß)
+        a = distance ennemy|impact = ennemy_speed * time
+        b = distance tower |impact = projectile_speed * (time - shot_time)
+        c = distance tower |ennemy
 
-        Returns:
-            [type]: [description]
+        cos(ß) = angle tower | ennemy_direction
+
+        => a * t² + b * t + c = 0
         """
         delta_x = self.absolute_position[0] - target.absolute_position[0]
         delta_y = self.absolute_position[1] - target.absolute_position[1]
         Dist2 = delta_x ** 2 + delta_y ** 2
 
-        # Cos Angle Tour|Dir Ennemi
         Cos_Beta = target.direction[0] * delta_x + target.direction[1] * delta_y
 
-        # Triangle Tour/Ennemi/Impact
-        # B^2 = A^2 + C^2 - 2AC cos(Beta)
-        # A = Distance Ennemi|Impact = Vitesse Ennemi * temps
-        # B = Distance Tour|Impact = Vitesse Projectile * (temps - tempsTir)
-        # C = Distance Tour|Ennemi
-        # Equation second ° pour T : a * t^2 + b * t + c = 0
-        a = self.projectile_model["speed"] ** 2 - target.speed ** 2
-        b = (2 * target.speed * Cos_Beta) - (2 * self.projectile_model["speed"] ** 2 * self.t0)
-        c = (self.projectile_model["speed"] ** 2 * self.t0 ** 2) - Dist2
+        a = (self.projectile_model["speed"] ** 2) - target.speed ** 2
+        b = (2 * target.speed * Cos_Beta) - (2 * (self.projectile_model["speed"] ** 2) * self.t0)
+        c = (self.projectile_model["speed"] ** 2) * (self.t0 ** 2) - Dist2
 
-        Ro = b ** 2 - 4 * a * c
+        Ro = (b ** 2) - (4 * a * c)
 
         t = (-b + math.sqrt(Ro)) / (2 * a)
 
         return t
 
     def setPosition(self, position: tuple):
+        """Sets the position of the tower (used when the tower is in the "hand" of the player)"""
         self.position = position
