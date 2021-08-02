7197b02e17b44f5c31cf0cd8e1daabf00c49398d
Minigrim0 <grimauflorent@gmail.com>
2021-08-02 23:28:46 +0200

    [Upd] gold animation objects

--- src/gold.py
+++ src/gold.py
@@ -1,32 +1,53 @@
-import pygame
 import math
 
+import pygame as pg
+
 from models.screen import Screen
+from models.game_options import GameOptions
+from models.image_bank import ImageBank
 
 
-class GoldAnim:
+class Gold:
     """A flying gold image to show the winned gold when an ennemy has been killed"""
 
     def __init__(self, pos: tuple, amount: int):
-        self.posx = pos[0]
-        self.posy = pos[1]
+        self.posx, self.posy = pos
+        bank = ImageBank.getInstance()
+        if bank.exists("gold"):
+            self.image = bank["gold"]
+        else:
+            self.image = pg.image.load("assets/images/coin.png")
+            bank.set("gold", self.image)
+
         self.amount = amount
-        self.i = 0
-        myfont = pygame.font.SysFont("Viner Hand ITC", 15)
-        self.NbrsAffiche = myfont.render(str(self.amount), 1, (0, 0, 0))
+        self.life_span = 2  # seconds
+        font = GameOptions.getInstance().fonts["MedievalSharp-xOZ5"]["14"]
+        self.NbrsAffiche = font.render(str(self.amount), 1, (0, 0, 0))
 
-    def bouge(self, screen: Screen, goldImg: pygame.Surface, niveau):
-        """Makes the gold coins move on the screen and disappear after a while
+    def update(self, timeElapsed):
+        """Updates the position and life_span of the gold coin
 
-        Args:
-            screen (Screen): The screen for the coin to display itself
-            goldImg (pygame.Surface): The image of the coin
-            niveau (Niveau): The level object for the coin to remove
-                itself from its gold array
+        Returns:
+            bool: whether the animation is over or not
         """
-        self.i += 1
-        self.posy -= 2
-        if self.i == 24:
-            niveau.GoldTab.remove(self)
-        screen.blit(goldImg, (self.posx - 12 + 3 * math.cos(self.i), self.posy - 6))
-        screen.blit(self.NbrsAffiche, (self.posx + 3 * math.cos(self.i), self.posy - 6))
+        self.life_span -= timeElapsed
+        self.posy -= 64 * timeElapsed
+        if self.life_span <= 0:
+            return True
+
+    def draw(self, screen: Screen):
+        """Draws the coin on the screen"""
+        screen.blit(
+            self.image,
+            (
+                self.posx - 12 + 3 * math.cos(10 * self.life_span),
+                self.posy - 6
+            )
+        )
+        screen.blit(
+            self.NbrsAffiche,
+            (
+                self.posx + 3 * math.cos(10 * self.life_span),
+                self.posy - 6
+            )
+        )
