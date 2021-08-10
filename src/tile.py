e51279f891613ad4be953a3eb2fe9a9e135f8c93
Minigrim0 <grimauflorent@gmail.com>
2021-08-11 00:16:26 +0200

    [Fix] unnecessary else/elif after return

--- src/tile.py
+++ src/tile.py
@@ -34,13 +34,13 @@ class Tile:
     def direction(self):
         if (self.code, self.rotation) in (("c1", 0), ("t1", 2), ("t2", 3)):
             return (1, 0)
-        elif (self.code, self.rotation) in (("c1", 1), ("t1", 3), ("t2", 0)):
+        if (self.code, self.rotation) in (("c1", 1), ("t1", 3), ("t2", 0)):
             return (0, -1)
-        elif (self.code, self.rotation) in (("c1", 2), ("t1", 0), ("t2", 1)):
+        if (self.code, self.rotation) in (("c1", 2), ("t1", 0), ("t2", 1)):
             return (-1, 0)
-        elif (self.code, self.rotation) in (("c1", 3), ("t1", 1), ("t2", 2)):
+        if (self.code, self.rotation) in (("c1", 3), ("t1", 1), ("t2", 2)):
             return (0, 1)
-        elif self.code == "x1":
+        if self.code == "x1":
             return (0, 0)
 
     def move(self, pos):
