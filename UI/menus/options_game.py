4ec557ce67f8ee4288e252ebe83d61825c98fd3d
Minigrim0 <grimauflorent@gmail.com>
2021-08-10 22:05:48 +0200

    [Upd] in game option menu

--- UI/menus/options_game.py
+++ UI/menus/options_game.py
@@ -1,43 +1,111 @@
-    # Menu_Principal d'Options dans menu pause
-    while OptionsMenu:
+import pygame as pg
 
-        game.play_music()
+from models.game_options import GameOptions
+from models.screen import Screen
+from src.runnable import Runnable
+from UI.components.button import Button
+from UI.menus.menu import Menu
 
-        pygame.mixer.music.set_volume(Volume / 10)
 
-        appuye = False
+class GameOptionsMenu(Menu, Runnable):
+    """Represents the in game option menu"""
 
-        Volumetxt = myfont3.render("Volume : %i" % int(Volume * 10), 1, (255, 50, 20))
-        Diffictxt = myfont3.render("Difficulté : %i" % Difficulte, 1, (255, 50, 20))
+    def __init__(self):
+        self.menu_background: pg.Surface = None
+        self.menu_background_position: tuple = None
 
-        screen.blit(Fond_Menu_Opt, (386, 142))
-        screen.blit(OptionsTxt, (386, 132))
-        screen.blit(Volumetxt, (410, 302))
-        screen.blit(Diffictxt, (410, 347))
-        screen.blit(Moins, (655, 302))
-        screen.blit(Plus, (705, 302))
-        screen.blit(Moins, (655, 347))
-        screen.blit(Plus, (705, 347))
-        screen.blit(quitpaus, (516, 407))
-        screen.flip()
+        self.volume_text: pg.Surface = None
+        self.difficulty_text: pg.Surface = None
 
-        for event in screen.getEvent():
+        self._build()
 
-            if event.type == pygame.locals.MOUSEBUTTONDOWN:
+    def _build(self):
+        """Builds the menu's buttons and texts"""
+        options = GameOptions.getInstance()
 
-                # Quitter le niveau en cours
-                if quitOrect.collidepoint(event.pos):
-                    OptionsMenu = False
-                    pausemenu = True
+        self.menu_background = pg.image.load(
+            options.fullPath("images", "backgrounds/submenu_background.png")
+        ).convert_alpha()
+        self.menu_background_position = (
+            (Screen.getInstance().get_size()[0] - self.menu_background.get_size()[0]) / 2,
+            (Screen.getInstance().get_size()[1] - self.menu_background.get_size()[1]) / 2
+        )
 
-                if VolPlus.collidepoint(event.pos) and Volume < 10:
-                    Volume += 1
+        title = options.fonts["MedievalSharp-xOZ5"]["60"].render(
+            "Options", 1, (0, 0, 0)
+        )
 
-                if VolMoins.collidepoint(event.pos) and Volume > 0:
-                    Volume -= 1
+        title_pos = (self.menu_background.get_size()[0] - title.get_size()[0]) / 2
 
-                if DifPlus.collidepoint(event.pos) and Difficulte < 10:
-                    Difficulte += 1
+        self.menu_background.blit(
+            title,
+            (title_pos, 15)
+        )
 
-                if DifMoins.collidepoint(event.pos) and Difficulte > 0:
-                    Difficulte -= 1
+        less = options.fonts["MedievalSharp-xOZ5"]["40"].render(
+            "-", 1, (0, 0, 0)
+        )
+        plus = options.fonts["MedievalSharp-xOZ5"]["40"].render(
+            "+", 1, (0, 0, 0)
+        )
+
+        self.buttons["lessVolume"] = Button((655, 302), (40, 40), image=less, callback=self.updateVolume, value=-1)
+        self.buttons["moreVolume"] = Button((705, 302), (40, 40), image=plus, callback=self.updateVolume, value=1)
+        self.buttons["lessDifficulty"] = Button(
+            (655, 347), (40, 40), image=less, callback=self.updateDifficulty, value=-1)
+        self.buttons["moreDifficulty"] = Button(
+            (705, 347), (40, 40), image=plus, callback=self.updateDifficulty, value=1)
+        self.buttons["quitOptions"] = Button(
+            (516, 407), (120, 50),
+            image=pg.image.load(options.fullPath("images", "buttons/small_button.png")).convert_alpha(),
+            callback=self.quitMenu
+        )
+        self.buttons["quitOptions"].build(
+            "Retour", options.fonts["MedievalSharp-xOZ5"]["25"],
+            text_position=("CENTER", "CENTER")
+        )
+
+        self._buildVolumeText()
+        self._buildDifficultyText()
+
+    def _buildVolumeText(self):
+        options = GameOptions.getInstance()
+        self.volume_text = options.fonts["MedievalSharp-xOZ5"]["25"].render(
+            f"Volume : {int(options.volume * 10)}", 1, (0, 0, 0)
+        )
+
+    def _buildDifficultyText(self):
+        options = GameOptions.getInstance()
+        self.difficulty_text = options.fonts["MedievalSharp-xOZ5"]["25"].render(
+            f"Difficulté : {options.difficulty}", 1, (0, 0, 0)
+        )
+
+    def loop(self):
+        super().loop()
+
+        self.draw()
+        self.screen.flip()
+
+        self.handleEvent()
+
+    def _draw(self):
+        self.screen.draw(self.menu_background, self.menu_background_position)
+        self.screen.blit(self.volume_text, (410, 302))
+        self.screen.blit(self.difficulty_text, (410, 347))
+
+    def handleEvent(self):
+        for _ in super().handleEvent():
+            pass
+
+    def updateVolume(self, value: int):
+        """Updates the volume of the music"""
+        GameOptions.getInstance().changeVolume(value)
+        self._buildVolumeText()
+
+    def updateDifficulty(self, value: int):
+        """Updates the difficulty of the game"""
+        GameOptions.getInstance().changeDifficulty(value)
+        self._buildDifficultyText()
+
+    def quitMenu(self):
+        self.running = False
