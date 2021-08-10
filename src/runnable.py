2207f003ee8eecee93815c83c2246247551164dd
Minigrim0 <grimauflorent@gmail.com>
2021-08-10 21:02:12 +0200

    [Add] allow arguments in runnable + implemented _start method

--- src/runnable.py
+++ src/runnable.py
@@ -1,18 +1,27 @@
+import logging
+
+
 class Runnable:
     """A runnable class, with a hidden loop, callable"""
 
     def __init__(self):
         self.running: bool = False
 
-    def __call__(self):
-        self.run()
+    def __call__(self, *args, **kwargs):
+        self.run(*args, **kwargs)
+
+    def _start(self, *args, **kwargs):
+        """The method called before the runnable loop is launched"""
+        logging.warning("Calling not implemented method _start")
 
     def _end(self):
-        pass
+        """The method called before the runnable loop ends"""
+        logging.warning("Calling not implemented method _end")
 
-    def run(self):
+    def run(self, *args, **kwargs):
         """Starts the loop"""
         self.running = True
+        self._start(*args, **kwargs)
         while self.running:
             self.loop()
         self._end()
