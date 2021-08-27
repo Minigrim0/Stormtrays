37c1750b192028ea144118ce15a535adab6b8f84
Minigrim0 <grimauflorent@gmail.com>
2021-08-27 18:53:23 +0200

    [Fix] addBox method

--- UI/components/power_bar/power_bar.py
+++ UI/components/power_bar/power_bar.py
@@ -1,3 +1,5 @@
+import logging
+
 import pygame as pg
 
 from UI.components.power_bar.box import Box
@@ -38,14 +40,13 @@ class PowerBar:
                 )
 
     def addBox(self, icon: pg.Surface, name: str, cooldown: int, callback: callable, **ckwargs):
+        logging.info(f"Adding '{name}' to power bar")
         self.boxes.append(
-            Box(
-                icon, callback
-            )
+            Box(icon, self.box_size, cooldown, callback, **ckwargs)
         )
 
     def draw(self, screen):
-        screen.blit(self.image, (0, 0))
+        screen.blit(self.image, self.position)
         for index, box in enumerate(self.boxes):
             box.draw(
                 screen, (self.position[0] + (self.box_size * index), self.position[1])
