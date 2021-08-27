13aa4f798b64d401ed6a6cefea79db5a5c2239ea
Minigrim0 <grimauflorent@gmail.com>
2021-08-27 18:44:37 +0200

    [Add] power bar UI component

---
+++ UI/components/power_bar/box.py
@@ -0,0 +1,48 @@
+import pygame as pg
+
+from models.game_options import GameOptions
+
+
+class Box:
+    """Represents a box of the power bar"""
+
+    def __init__(self, icon: pg.Surface, size: int, cooldown: int, callback: callable, **ckwargs):
+        self.icon = icon
+        self.size = size
+
+        self.initial_cooldown = cooldown
+        self.current_cooldown = 0
+        self.cooldown_text: pg.Surface = None
+
+        self.callback: callable = callback
+        self.ckwargs = ckwargs
+
+    @property
+    def available(self) -> bool:
+        return self.current_cooldown == 0
+
+    @property
+    def _cooldownText(self) -> pg.Surface:
+        options = GameOptions.getInstance()
+        return options.fonts["MedievalSharp-xOZ5"]["14"].render(
+            self.current_cooldown, 0, (255, 255, 255)
+        )
+
+    def trigger(self):
+        if self.available:
+            if self.callback(**self.ckwargs):
+                self.current_cooldown = self.initial_cooldown
+
+    def update(self, elapsed_time: float):
+        if not self.available:
+            self.current_cooldown = max(self.current_cooldown - elapsed_time, 0)
+
+    def draw(self, screen, position: tuple):
+        screen.blit(self.icon, position)
+        screen.blit(
+            self._cooldownText,
+            (
+                position[0],
+                position[1] + self.size - 14
+            )
+        )
---
+++ UI/components/power_bar/power_bar.py
@@ -0,0 +1,60 @@
+import pygame as pg
+
+from UI.components.power_bar.box import Box
+
+
+class PowerBar:
+    """The character's power bar"""
+
+    def __init__(
+        self, position: tuple = ("CENTERED", "BOTTOM"), rows: int = 1, columns: int = 5,
+        box_size: int = 32
+    ):
+        self.box_size = box_size
+        self.columns = columns
+        self.rows = rows
+        self.boxes: [Box] = []
+
+        self.image: pg.Surface = None
+        self._build()
+
+    def _build(self):
+        """Builds the UI of the Power Bar"""
+        total_size = (
+            self.box_size * self.columns,
+            self.box_size * self.rows
+        )
+        self.image = pg.Surface(total_size, pg.SRCALPHA)
+        self.image.fill((0, 0, 0, 128))
+        for row in range(self.rows):
+            for column in range(self.columns):
+                pg.draw.rect(
+                    self.image, (255, 255, 255),
+                    pg.Rect(
+                        (column * self.box_size, row * self.box_size),
+                        (self.box_size, self.box_size)
+                    ),
+                    width=1
+                )
+
+    def addBox(self, icon: pg.Surface, name: str, cooldown: int, callback: callable, **ckwargs):
+        self.boxes.append(
+            Box(
+                icon, callback
+            )
+        )
+
+    def draw(self, screen):
+        screen.blit(self.image, (0, 0))
+        for index, box in enumerate(self.boxes):
+            box.draw(
+                screen, (self.position[0] + (self.box_size * index), self.position[1])
+            )
+
+    def update(self, elapsed_time: float):
+        for box in self.boxes:
+            box.update(elapsed_time)
+
+    def handleEvent(self, event):
+        if event.type == pg.KEYDOWN and event.key == pg.K_1:
+            self.boxes[0].trigger()
