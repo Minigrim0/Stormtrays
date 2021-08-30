b542f3e25da0051615b67e5b4384a7bb2baa20ae
Minigrim0 <grimauflorent@gmail.com>
2021-08-30 19:26:47 +0200

    [Add] tower sell method

--- src/tower.py
+++ src/tower.py
@@ -1,10 +1,10 @@
-import logging
 import math
 
 import pygame
 
 from models.ennemy import Ennemy
 from models.game_options import GameOptions
+from models.level import Level
 from models.projectile import Projectile
 from models.screen import Screen
 from src.ennemy import EnnemyDO
@@ -191,7 +191,6 @@ class TowerDO:
     def click(self, position: tuple):
         """Sets the current tower as selected or not depending on whether the click is on the tower or not"""
         absolute_position = self.absolute_position
-        print(absolute_position, position, self.size)
         if (
             absolute_position[0] <= position[0] <= absolute_position[0] + self.size[0] and
             absolute_position[1] <= position[1] <= absolute_position[1] + self.size[1]
@@ -202,4 +201,11 @@ class TowerDO:
 
     def sell(self):
         """Gives back a percentage of the tower cost and destroys the tower"""
-        logging.info("Selling tower")
+        level = Level.getInstance()
+        level.addGold(
+            int(self.price * 0.75),
+            self.centered_position
+        )
+
+        from models.tower import Tower
+        Tower.getInstance().delTower(self)
