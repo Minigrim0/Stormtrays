93c81dad2656fb83972fd3c18912c89b2150b808
Minigrim0 <grimauflorent@gmail.com>
2021-09-25 21:38:50 +0200

    [Upd] move manage script

---
+++ manage.py
@@ -0,0 +1,47 @@
+import sys
+import json
+import subprocess
+
+command = sys.argv[1]
+
+
+def makeTrans():
+    """Creates the POT files and the po files from the code"""
+    with open("locales/settings.json") as settings_file:
+        apps = json.load(settings_file)["apps"]
+
+    for app_name, file in apps.items():
+        subprocess.run(
+            ["/usr/lib/python3.9/Tools/i18n/pygettext.py", "-d", app_name, "-o", f"locales/{app_name}.pot", file]
+        )
+
+
+def compileTrans():
+    """Compiles all the po files into mo files"""
+    pass
+
+
+def addApp():
+    """Adds an app to be translated"""
+    if len(sys.argv) < 4:
+        raise RuntimeError("Missing parameters for command addapp")
+    app = sys.argv[2]
+    path = sys.argv[3]
+
+    with open("locales/settings.json", "r") as settings_file:
+        data = json.load(settings_file)
+        data["apps"][app] = path
+    with open("locales/settings.json", "w") as settings_file:
+        json.dump(data, settings_file)
+
+
+availabe_commands = {
+    "maketrans": makeTrans,
+    "compiletrans": compileTrans,
+    "addapp": addApp
+}
+
+if command not in availabe_commands:
+    raise NotImplementedError(f"command {command} not found !")
+
+availabe_commands[command]()
